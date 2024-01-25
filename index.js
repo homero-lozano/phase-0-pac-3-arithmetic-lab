966e264f2a5e63ed2fad8e463e52587cf002334e
Nico Lozano <homeronicolaslozano@gmail.com>
2024-01-24 19:46:39 -0500

    Adds myfiles.md

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a, b) { 
+    return a + b 
+} 
+function subtract(a, b) {
+    return a - b 
+} 
+function multiply(a, b) {
+    return a * b
+} 
+function divide(a, b) {
+    return a / b 
+} 
+function increment(n) {
+    return n + 1 
+}
+function decrement(n) {
+    return n - 1
+}
+function makeInt(n) {
+let Int = parseInt(n, 10) 
+return Int 
+}
+function preserveDecimal(n) {
+    let Decimal = parseFloat(n, 2.222)
+    return Decimal
+}
